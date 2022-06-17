5f6435fc0e34499097e7fbbe05e07b5dc002bce7
Adrizen <36826976+Adrizen@users.noreply.github.com>
2022-06-17 13:42:28 -0300

    Email de bienvenida al nuevo user registrado.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,19 +1,22 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const sgMail = require('@sendgrid/mail')
+require('dotenv').config();
 
+// Registrar un usuario nuevo.
 async function register(req, res) {
     try {
         // Obtener input.
         const { nombre, email, password } = req.body;
         // Validar input del usuario.
-        if (!(email && password && nombre )){
+        if (!(email && password && nombre)) {
             res.status(400).send("Se requiere rellenar todos los campos: nombre, mail y password");
         }
 
         // Checkear si el usuario ya existe.
         const oldUser = await User.findOne({ where: { email: email } });
-        if (oldUser){
+        if (oldUser) {
             return res.status(409).send("Este email ya está registrado.")
         }
 
@@ -26,6 +29,8 @@ async function register(req, res) {
             email: email.toLowerCase(), // Convertir el mail a minúsculas.
             password: encryptedPassword
         });
+
+        enviarMail(email);  // Enviar email de bienvenida al nuevo usuario registrado.
 
         // Crear token.
         const token = jwt.sign({ user_id: user.id, email }, "secret", { expiresIn: "10h" });
@@ -42,20 +47,37 @@ async function register(req, res) {
     }
 }
 
+// Si un nuevo usuario se registra con éxito, se le envía un email de bienvenida.
+function enviarMail(emailUsuario) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY) 
+    const msg = {
+        to: emailUsuario, // Receptor.
+        from: 'lidar54583@runqx.com', // Sender verificado.
+        subject: '¡Bienvenido a Mundo Disney!',
+        text: 'Esperamos que disfrutes de nuestra API'
+    }
+    sgMail.send(msg).then(() => {
+        console.log('Email de bienvenida enviado.')
+    }).catch((error) => {
+        console.error(error)
+    });
+}
+
+// Login de usuario existente.
 async function login(req, res) {
     try {
         // Obtener input.
         const { email, password } = req.body;
 
         // Validar input.
-        if (!(email && password)){
+        if (!(email && password)) {
             res.status(400).send("Se requiere rellenar todos los campos: email y password.")
         }
 
         // Validar si el usuario existe en la DB.
         const user = await User.findOne({ where: { email: email } });
 
-        if (user && (await bcrypt.compare(password, user.password))){
+        if (user && (await bcrypt.compare(password, user.password))) {
 
             // Crear token.
             const token = jwt.sign({ user_id: user.id, email }, "secret", { expiresIn: "10h" });
